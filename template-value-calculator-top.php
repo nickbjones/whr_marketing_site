f2536386dd679e39b2c24d0ec04987a25ec10b90
nickbjones <jonesnickb@gmail.com>
2021-05-07 07:53:12 +0900

    fixing various frontend issues

--- template-value-calculator-top.php
+++ template-value-calculator-top.php
@@ -222,32 +222,27 @@
   <script>
     $(document).ready(function() {
       $('[data-onchange').on('change', function() {
-        // get step 1 inputs
+        // get inputs
         const companyName = $('#company-name').val();
-        const noOfEmployees = parseInt($('#number-of-employees').val());
-        const staffAvgHourlyWage = parseInt($('#staff-avg-hourly-wage').val());
-        const numberOfAnnualContractRenewals = parseInt($('#number-of-annual-contract-renewals').val());
-          
-        // get step 2 inputs
-        const contractCreationTime = parseInt($('#contract-creation-time').val());
-        const bookBindingSealingTime = parseInt($('#book-binding-sealing-time').val());
-        const shippingTime = parseInt($('#shipping-time').val());
-          
-        // get step 3 inputs
-        const dataEntry = parseInt($('#data-entry').val());
-        const contractFiling = parseInt($('#contract-filing').val());
+        const noOfEmployees = parseInt($('#number-of-employees').val()) || 0;
+        const staffAvgHourlyWage = parseInt($('#staff-avg-hourly-wage').val()) || 0;
+        const numberOfAnnualContractRenewals = parseInt($('#number-of-annual-contract-renewals').val()) || 0;
+        const contractCreationTime = parseInt($('#contract-creation-time').val()) || 0;
+        const bookBindingSealingTime = parseInt($('#book-binding-sealing-time').val()) || 0;
+        const shippingTime = parseInt($('#shipping-time').val()) || 0;
+        const dataEntry = parseInt($('#data-entry').val()) || 0;
+        const contractFiling = parseInt($('#contract-filing').val()) || 0;
 
-        // calc
+        // run calculations
         const sumOfTimes = contractCreationTime + bookBindingSealingTime + shippingTime + dataEntry + contractFiling;
         const timeNow = noOfEmployees * sumOfTimes * numberOfAnnualContractRenewals;
         const timeWelcomeHr = noOfEmployees * 3 * numberOfAnnualContractRenewals;
-
         const currentTimeHours = Math.floor(timeNow / 60);
         const currentTimeMinutes = timeNow % 60;
         const welcomehrTimeHours = Math.floor(timeWelcomeHr / 60);
         const welcomehrTimeMinutes = timeWelcomeHr % 60;
 
-        // set vals
+        // set values to send
         $('#form-company-name').val(companyName);
         $('#form-number-of-employees').val(noOfEmployees);
         $('#form-staff-avg-hourly-wage').val(staffAvgHourlyWage);
@@ -258,6 +253,7 @@
         $('#form-data-entry').val(dataEntry);
         $('#form-contract-filing').val(contractFiling);
 
+        // set values in bottom block
         $('#calculator-company-name').html(companyName);
         $('#calculator-current-time-hours').text(currentTimeHours);
         $('#calculator-current-time-minutes').text(currentTimeMinutes);
